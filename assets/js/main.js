1ea6a9646e4b1115efd2f16e9aefec3c0848ce42
thanhtuanmystic <tuanvp2001@gmail.com>
2024-06-18 16:50:12 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ let navbar = document.querySelector(".navbar");
 let searchBox = document.querySelector(".search-box .bx-search");
 // let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
-
 // sidebar open close js code
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
@@ -21,11 +20,52 @@ let htmlcssArrow = document.querySelector(".htmlcss-arrow");
 htmlcssArrow.onclick = function () {
   navLinks.classList.toggle("show1");
 };
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function () {
-  navLinks.classList.toggle("show2");
-};
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function () {
-  navLinks.classList.toggle("show3");
-};
+// let moreArrow = document.querySelector(".more-arrow");
+// moreArrow.onclick = function () {
+//   navLinks.classList.toggle("show2");
+// };
+// let jsArrow = document.querySelector(".js-arrow");
+// jsArrow.onclick = function () {
+//   navLinks.classList.toggle("show3");
+// };
+
+// Solution
+$(document).ready(function () {
+  // Xử lý click ẩn hiện ở phần sản phẩm trang chủ
+  $("#solution1").show();
+  $(".solution-click").click(function () {
+    $(".solution-click").removeClass("solution-active");
+    $(this).addClass("solution-active");
+    var contentId = $(this).data("content");
+    $(".solution-content").hide();
+    $("#" + contentId).show();
+  });
+});
+
+// Scroll Partner
+const scrollers = document.querySelectorAll(".scroller");
+
+// If an user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
